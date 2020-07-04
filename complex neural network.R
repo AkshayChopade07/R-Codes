38ed91d9bef0e390255eda96b6256ea305955f21
AkshayChopade07 <63453600+AkshayChopade07@users.noreply.github.com>
2020-07-04 18:48:19 +0530

    Complex Neural Network
    Neural networks are a set of algorithms, modeled loosely after the human brain, that are designed to recognize patterns. They interpret sensory data through a kind of machine perception, labeling or clustering raw input.

---
+++ complex neural network.R
@@ -0,0 +1,13 @@
+## Improving model performance ----
+# a more complex neural network topology with 5 hidden neurons
+concrete_model2 <- neuralnet(strength ~ cement + slag +
+                               ash + water + superplastic + 
+                               coarseagg + fineagg + age,
+                             data = concrete_train, hidden =c(5,2))
+# plot the network
+windows();
+plot(concrete_model2)
+# evaluate the results as we did before
+model_results2 <- compute(concrete_model2, concrete_test[1:8])
+predicted_strength2 <- model_results2$net.result
+cor(predicted_strength2, concrete_test$strength)
