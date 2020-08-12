d1552f00737477ab3b78a88efec867e0608a561d
AkshayChopade07 <63453600+AkshayChopade07@users.noreply.github.com>
2020-08-12 19:55:22 +0530

    KNN  Algorithm
    The k-nearest neighbors (KNN) algorithm is a simple, supervised machine learning algorithm that can be used to solve both classification and regression problems. It's easy to implement and understand, but has a major drawback of becoming significantly slows as the size of that data in use grows.

---
+++ knn.R
@@ -0,0 +1,28 @@
+wbcd<-read.csv("C:/Users/Immortal/Documents/R/wbcd_new.csv")
+# table of diagnosis
+prop.table(table(wbcd$diagnosis))*100
+# recode diagnosis as a factor
+wbcd$diagnosis <- factor(wbcd$diagnosis, levels = c("B", "M"),
+                         labels = c("Benign", "Malignant"))
+# create training and test data
+wbcd_train <- wbcd[1:469, ]
+wbcd_test <- wbcd[470:569, ]
+
+# create labels for training and test data
+
+wbcd_train_labels <- wbcd[1:469, 1]
+wbcd_test_labels <- wbcd[470:569, 1]
+
+#---- Training a model on the data ----
+
+# load the "class" library
+install.packages("class")
+library(class)
+
+wbcd_test_pred <- knn(train = wbcd_train[,-1], 
+                      test = wbcd_test[,-1],
+                      cl = wbcd_train_labels, k=30)
+##--------Evaluating model performance ----
+# Create the cross tabulation of predicted vs. actual
+table(wbcd_test_labels,wbcd_test_pred)
+
