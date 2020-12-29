4ee64e4c7515fe328dbea98090fda439741af0ab
Aksahy Chopade <63453600+AkshayChopade07@users.noreply.github.com>
2020-12-29 12:38:52 +0530

    ANCOVA
    The simple regression analysis gives multiple results for each value of the categorical variable. In such scenario, we can study the effect of the categorical variable by using it along with the predictor variable and comparing the regression lines for each level of the categorical variable. Such an analysis is termed as Analysis of Covariance also called as ANCOVA.

---
+++ ANCOVA.R
@@ -0,0 +1,26 @@
+input <- mtcars[,c("am","mpg","hp")]
+print(head(input))
+#Live Demo
+# Get the dataset.
+input <- mtcars
+
+# Create the regression model.
+result <- aov(mpg~hp*am,data = input)
+print(summary(result))
+# Get the dataset.
+input <- mtcars
+
+# Create the regression model.
+result <- aov(mpg~hp+am,data = input)
+print(summary(result))
+
+# Get the dataset.
+input <- mtcars
+
+# Create the regression models.
+result1 <- aov(mpg~hp*am,data = input)
+result2 <- aov(mpg~hp+am,data = input)
+
+# Compare the two models.
+print(anova(result1,result2))
+
